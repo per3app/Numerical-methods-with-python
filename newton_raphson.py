4a4abf18f7d1a00cc0a85b910187c3ac2ff279b1
Geomar Paul <per3app@gmail.com>
2018-06-15 12:49:11 -0500

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -3,10 +3,11 @@ import numpy as np
 
 n=8             #limite del eje x
 X=np.zeros((n))     #vector vacio del eje x
-Y=np.zeros((n))
+
 F=np.zeros((n))
 DF=np.zeros((n))
 Er=np.zeros((n))
+
 print('-----------------------------')
 print('    X           Error')
 for i in range(n-1):
