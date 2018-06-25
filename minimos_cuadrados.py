757a5c6f0799c4757e6bb6e0776015ffa524b5f5
Geomar Paul <per3app@gmail.com>
2018-06-25 10:50:06 -0500

    Add files via upload

---
+++ minimos_cuadrados.py
@@ -0,0 +1,38 @@
+#ajuste por minimos cuadrados
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+Y=[1.4,2.65,3.21,4.55,5.87,6.12,7.09,8.33,9.21,10.67]
+L=len(Y)
+
+X=np.zeros((L))
+
+sx=0
+sx2=0
+sy=0
+sxy=0
+
+for i in range(L):
+    X[i]=i+1
+    sx=sx+X[i]
+    sx2=sx2+X[i]**2
+    sy=sy+Y[i]
+    sxy=sxy+X[i]*Y[i]
+
+a=(L*sxy-sx*sy)/(L*sx2-sx**2)
+b=(sy-a*sx)/L
+
+Ya=np.zeros((L))
+for i in range(L):
+    Ya[i]=a*X[i]+b
+
+plt.figure(1)
+plt.title('Ajuste por minimos cuadrados')
+plt.xlabel('Eje X')
+plt.ylabel('Eje Y')
+plt.plot(X,Y,'r')
+plt.plot(X,Ya,'k+')
+plt.legend(['Datos','Datos ajustados'])
+plt.savefig('D:/minimos_cuadrados.png')
+
